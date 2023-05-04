88c66d69acf186c98b62463aa5613263c81a5401
Juan Diego Arocha <gestor.juandiego@gmail.com>
2023-05-04 08:15:36 +0200

    done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+// hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -21,5 +22,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+//TODO hice ambos metodos, queria probar primero el async await y luego hice como hicimos en el codigo de clase con Jorge a 1.5
+
+// app.get("/beers", async function (req, res) {
+//   try {
+//     const beersFromApi = await punkAPI.getBeers();
+//     res.render("beers", { beers: beersFromApi});
+//   } catch ( error ) {
+//     console.log(error)
+//   }
+// })
+
+app.get('/beers', (req, res) => {
+  console.log("Beers from API")
+  punkAPI.getBeers()
+    .then((beersFromApi) => {
+      console.log(beersFromApi)
+      res.render("beers", { 
+      beers: beersFromApi
+    });
+  })
+    .catch((error) => {
+      console.log(error);
+      res.render("error");
+    });
+});
+
+app.get("/random-beer", async (req, res) => {
+  try {
+    const randomBeer = await punkAPI.getRandom();
+    console.log(randomBeer)
+    res.render("random-beer.hbs", {
+      randomBeer
+    })
+  } catch ( error ) {
+    console.log(error)
+    res.render("error")
+  }
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
